0c1a1062e113cae5575e91f3a33ccbbd753eac88
igor-matsuzaki-movile <igor.matsuzaki@wavy.global>
2019-03-29 23:20:47 -0300

    first commit

---
+++ shorten_api/config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :shorten_api,
+  ecto_repos: [ShortenApi.Repo]
+
+# Configures the endpoint
+config :shorten_api, ShortenApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "YIh934bEiwlbd4hqyI3vbpy8y7MngwNFfKHFR+lzHqoQhHNWNqKHW6AUJ0l5yaAW",
+  render_errors: [view: ShortenApiWeb.ErrorView, accepts: ~w(json)],
+  pubsub: [name: ShortenApi.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
